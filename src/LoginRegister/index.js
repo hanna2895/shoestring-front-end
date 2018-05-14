53a6bcd34a407974c8c632c634e6182fd418dfca
Samat Baltin <somych1@gmail.com>
2018-05-14 11:34:50 -0500

    created LoginRegister folder

---
+++ src/LoginRegister/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class LoginRegister extends Component{
+	render(){
+		console.log('this is login')
+		return(
+			"hello"
+		)
+	}
+}
+
+export default LoginRegister;

ae8041df2b11877378427acf8110243b1cda52c9
Samat Baltin <somych1@gmail.com>
2018-05-14 14:25:04 -0500

    123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import LoginRegister from './LoginRegister'
-=======
 import UserSidebar from './UserSidebar'
 import AllTripsContainer from './AllTripsContainer';
-
->>>>>>> 43566a425a53fbf99b8b0b59c7d5c915e029f233
 
 class App extends Component {
   constructor(){

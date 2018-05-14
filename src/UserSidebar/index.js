339e0eeec4a8e516d4304426bc9c5a3a3fae214d
Zoe <zoereagan@gmail.com>
2018-05-14 12:24:51 -0500

    reactstrap module added; presentational user component started

---
+++ src/UserSidebar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import UserSideCard from '../UserSidecard'
+
+class UserSidebar extends Component {
+  render() {
+    return(
+      <UserSideCard />
+    )
+  }
+}
+
+export default UserSidebar;

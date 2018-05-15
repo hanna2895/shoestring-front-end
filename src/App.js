5ff5d14790a61927ef5e042b32c6fd9a02541ce5
Samat Baltin <somych1@gmail.com>
2018-05-15 15:06:42 -0500

    added user edit function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import LoginRegister from './LoginRegister'
 import UserSidebar from './UserSidebar'
 import AllTripsContainer from './AllTripsContainer';
-import UserEditModel from './UserEditModel'
+import UserEditModal from './UserEditModal'
 
 class App extends Component {
   constructor(){
@@ -11,13 +11,12 @@ class App extends Component {
     this.state = {
       loggedIn: false,
       loginError: '',
+      user_id: '',
       name: '',
       username: '',
-<<<<<<< HEAD
-      openModal: false
-=======
-      photo: ''
->>>>>>> d4197ba961ee44e087e1e9ba19efc0b3b2676ba3
+      photo: '',
+      openModal: false,
+      userEditError: ''
     }
   }
 
@@ -25,6 +24,7 @@ class App extends Component {
     this.showUserSidebar()
     .then((user) => {
       this.setState({
+        user_id: user.found_user.id,
         name: user.found_user.name,
         username: user.found_user.username,
         photo: user.found_user.photo
@@ -77,7 +77,6 @@ class App extends Component {
         loginError: registrationResponse.message
       })
     }
-
   }
 
   showUserSidebar = async () => {
@@ -92,8 +91,43 @@ class App extends Component {
     return user;
 
   }
-  openModal = (e) => {
-    console.log('This is openModal button')
+  openModal = async (e) => {
+    const userToEdit = await fetch('http://localhost:9292/user', {
+      credentials: 'include'
+    });
+    const foundUser = await userToEdit.json()
+    this.setState({
+      openModal: true,
+      userToEdit: foundUser.found_user
+    })
+
+  }
+  editUser = async (name, username, password, photo, id) => {
+    console.log(id, 'dsajhsfjhbshjkbakshdjhkadsbhjdksakbjhkhbjdfashjkbfdaskbhjf')
+    const user = await fetch('http://localhost:9292/user/' + id, {
+      method: 'PUT',
+      credentials: 'include',
+      body: JSON.stringify({
+        name: name,
+        username: username,
+        password: password,
+        photo: photo
+      })
+    });
+    const response = await user.json()
+    console.log(response.user)
+    if(response.success){
+      this.setState({
+        name: name,
+        username: username,
+        photo: photo,
+        openModal: false
+      })
+    } else {
+      this.setState({
+        userEditError: response.message
+      })
+    }
   }
 
   render() {
@@ -104,7 +138,8 @@ class App extends Component {
           <div>
             <h1>Hello!</h1>
             <div className="container">      
-              <UserSidebar username={this.state.username} name={this.state.name} photo={this.state.photo}/>
+              <UserSidebar username={this.state.username} name={this.state.name} photo={this.state.photo} openModal={this.openModal}/>
+              <UserEditModal modalState={this.state.openModal} user_id={this.state.user_id} user_name={this.state.name} username={this.state.username} photo={this.state.photo} editUser={this.editUser} userEditError={this.state.userEditError}/>
               <AllTripsContainer />
             </div>
           </div>
